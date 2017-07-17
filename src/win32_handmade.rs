54d6845f64d0db4c3eb70661724298a30e687033
Wheeb <ngrattan1984@googlemail.com>
2017-07-17 23:29:46 +0100

    Got this painting with colour version

--- src/win32_handmade.rs
+++ src/win32_handmade.rs
@@ -2,17 +2,19 @@ extern crate user32;
 extern crate winapi;
 extern crate kernel32;
 extern crate libc;
+extern crate gdi32;
 use std::ffi::OsStr;
 // use std::io::Error;
 use std::iter::once;
 use std::os::windows::ffi::OsStrExt;
 // use std::ptr::null_mut;
 use std::mem;
-use self::winapi::winuser::{WNDCLASSW,CS_HREDRAW,CS_VREDRAW,WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,MSG,WS_VISIBLE,PM_REMOVE,WM_PAINT,WM_SIZE,WM_CLOSE,WM_ACTIVATEAPP};
+use self::winapi::winuser::{WNDCLASSW,CS_HREDRAW,CS_VREDRAW,WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,MSG,WS_VISIBLE,PM_REMOVE,WM_PAINT,WM_SIZE,WM_CLOSE,WM_ACTIVATEAPP,PAINTSTRUCT};
 use self::winapi::windef::{HWND,HBRUSH,HMENU};
 use self::winapi::minwindef::{UINT,WPARAM,LPARAM,LRESULT,LPVOID};
+use self::winapi::wingdi::{WHITENESS,BLACKNESS, PATINVERT};
 
-static mut GLOBAL_RUNNING: bool = true;
+static mut GLOBAL_RUNNING: bool = true; //TODO: Whats the correct way to scope something like this?
 
 pub fn main() {
 
@@ -34,7 +36,7 @@ pub fn main() {
         lpszClassName: window_class_name
     };
 
-    let ret = unsafe {
+    let ret = unsafe { //TODO: Can we use Options to get rid of all these unsafes?
         user32::RegisterClassW(&mut window)
     };
 
@@ -43,8 +45,8 @@ pub fn main() {
         return
     }
     
-    let hwnd = unsafe {
-        user32::CreateWindowExW(
+    unsafe { 
+         let window_handle = user32::CreateWindowExW(
             0,
             window_class_name,
             to_wide_string("Rust Hero").as_ptr(),
@@ -57,18 +59,16 @@ pub fn main() {
             0 as HMENU,
             h_instance,
             0 as LPVOID
-        )
-    };
-    
-    if hwnd.is_null() {
-        println!("Failed to create window.");
-        return;
-    }
+        );
 
-    unsafe {
+        if window_handle.is_null() {
+            println!("Failed to create window.");
+            return;
+        }
+
         while GLOBAL_RUNNING {
             let mut msg: MSG = mem::uninitialized();
-            while user32::PeekMessageW(&mut msg, hwnd,0,0,PM_REMOVE) != 0 {
+            while user32::PeekMessageW(&mut msg, window_handle,0,0,PM_REMOVE) != 0 {
                 user32::TranslateMessage(&mut msg);
                 user32::DispatchMessageW(&mut msg);
             }
@@ -80,16 +80,21 @@ fn to_wide_string(str: &str) -> Vec<u16> {
     OsStr::new(str).encode_wide().chain(once(0)).collect()
 }
 
- pub unsafe extern "system" fn winproc(h_wnd :HWND, msg :UINT, w_param :WPARAM, l_param :LPARAM) -> LRESULT {
+ pub unsafe extern "system" fn winproc(hwnd :HWND, msg :UINT, w_param :WPARAM, l_param :LPARAM) -> LRESULT {
         match msg {
             WM_SIZE => { println!("WM_SIZE"); },
             WM_CLOSE => {  GLOBAL_RUNNING = false; },
             WM_ACTIVATEAPP => { println!("WM_ACTIVATEAPP"); },
-            WM_PAINT => { 
-                println!("WM_PAINT"); 
-                return user32::DefWindowProcW(h_wnd, msg, w_param, l_param)
+            WM_PAINT => {
+                let mut paint: PAINTSTRUCT = mem::uninitialized();
+                let hdc = user32::BeginPaint(hwnd, &mut paint);
+                let x = paint.rcPaint.left;
+                let y = paint.rcPaint.top;
+                let width = paint.rcPaint.right - paint.rcPaint.left;
+                let height = paint.rcPaint.bottom - paint.rcPaint.top;
+                gdi32::PatBlt(hdc, x, y, width, height, PATINVERT);
             },
-            _ => { return user32::DefWindowProcW(h_wnd, msg, w_param, l_param); }
+            _ => { return user32::DefWindowProcW(hwnd, msg, w_param, l_param); }
         };
         0
     }
